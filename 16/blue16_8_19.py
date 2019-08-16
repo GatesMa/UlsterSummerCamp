b1dfc8f31c49c9a4835471982ba3554180b2558e
GatesMa <42234931+GatesMa@users.noreply.github.com>
2019-08-16 12:21:52 +0100

    Create blue16_8_19.py

---
+++ 16/blue16_8_19.py
@@ -0,0 +1,55 @@
+from bluepy.btle import BTLEException
+from bluepy import sensortag
+from bluepy.sensortag import SensorTag
+import time
+import datetime
+
+SENSORTAG_ADDRESS = "54:6C:0E:53:1E:3D"
+
+
+def enable_sensors(tag):
+    tag.IRtemperature.enable()
+    tag.lightmeter.enable()
+    time.sleep(1.0)
+
+def disable_sensors(tag):
+    tag.IRtemperature.disable()
+    tag.lightmeter.disable()
+
+def get_readings(tag):
+    try:
+        enable_sensors(tag)
+        readings = {}
+
+        readings["ir_temp_amb"], readings["ir_temp_obj"] = tag.IRtemperature.read()
+        readings["baro_temp"], readings["pressure"] = tag.barometer.read()
+
+        readings = {key: round(value, 2) for key, value in readings.items()}
+        return readings
+    except BTLEException as e:
+        print("Unable to take sensor readings.")
+        print(e)
+        return {}
+def reconnect(tag):
+    try:
+        tag.connect(tag.deviceAddr, tag.addrType)
+    except Exception as e:
+        print("Unable to reconnect to SensorTag.")
+        raise e
+    
+print('Connecting to {}'.format(SENSORTAG_ADDRESS))
+tag = SensorTag(SENSORTAG_ADDRESS)
+
+#disable_sensors(tag)
+
+while True:
+    readings = get_readings(tag)
+    if not readings:
+        print("SensorTag disconnected. Reconnecting.")
+        reconnect(tag)
+        continue
+    print("Time:\t{}".format(datetime.datetime.now()))
+    print("Temperature:", readings["ir_temp_obj"])
+    print("Pressure: ", readings["pressure"])
+    print('--------------------------------------')
+
